98959f9f48c5ea39ccc7d17e846d8feb3b03d217
mariale263 <mariale.contreras@gmail.com>
2018-02-09 18:06:38 +0100

    Agregar genero con ajax

--- routes/web.php
+++ routes/web.php
@@ -18,13 +18,11 @@ Route::get('/contacto', 'FrontController@contacto');
 Route::get('/reviews', 'FrontController@reviews');
 Route::get('/reviews', 'FrontController@reviews');
 Route::get('/admin', 'FrontController@admin');
-
 Route::resource('usuario', 'UsuarioController');
-
 Route::resource('log', 'LogController');
-
-
 Route::get('logout','LogController@logout');
+
+Route::resource('genero','GeneroController');
 
 //Route::get('movie', 'MovieController');
 
